25be3a48c4ef04ea632d20ddd5155ef6388084ac
tushar <tushnts@email.com>
2025-04-03 17:26:36 +0530

    added venv and requirements.txt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    
 db = SQLAlchemy(app)
 
 class User(db.Model):
